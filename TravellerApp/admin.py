0c6d1788051484a6a26cdd26cfecd4796f2b636d
Marius Emanuel <mariusemanuelnicusor@yahoo.com>
2023-03-21 18:08:04 +0000

    First view

--- TravellerApp/admin.py
+++ TravellerApp/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-from .models import Trip
+from .models import Trip, Comment
 from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 
 @admin.register(Trip)
 class TripAdmin(SummernoteModelAdmin):
-
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = 'content'
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'approved', 'created_on')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comment']
+
+    def approve_comment(self, request, queryset):
+        queryset.update(approved=True)
